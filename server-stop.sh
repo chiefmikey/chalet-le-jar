69a466527707b7a484f47faba2d2501cd43cc6f8
chiefmikey <wolfemikl@gmail.com>
2021-10-23 17:47:39 -0600

    Include aws config

--- server-stop.sh
+++ server-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 screen -S bedrock -X stuff 'save hold\n'
-date = echo $(date +%y:%m:%d:%H:%M:%S)
-git add ~/worlds && git commit -am echo $date
+currentDate=$(date +%y:%m:%d:%H:%M:%S)
+git add . && git commit -am $currentDate && git push origin main
 screen -S bedrock -X stuff 'save resume\n'
