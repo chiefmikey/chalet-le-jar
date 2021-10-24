529d32e32b7fb932a2e4dc984be4fe7e221252a4
chiefmikey <wolfemikl@gmail.com>
2021-10-24 06:03:55 +0000

    21:10:24:06:03:55

--- server-stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-screen -S bedrock -X stuff 'save hold\n'
-currentDate=$(date +%y:%m:%d:%H:%M:%S)
-git add . && git commit -am $currentDate && git push origin main
-screen -S bedrock -X stuff 'save resume\n'
