75b993aa4ba59128a07cc00225c4ea49120025b6
chiefmikey <wolfemikl@gmail.com>
2021-10-23 22:53:52 -0600

    Update gitignore

--- server-init.sh
+++ server-init.sh
@@ -6,23 +6,17 @@ sudo touch ~/$INSTANCE_ALREADY_STARTED
   echo "-- First instance startup --"
     sudo apt update -y
     sudo apt upgrade -y
-    sudo apt install -y wget unzip git gitsome jq
-    git config --global user.name "chiefmikey"
-    git config --global user.email "wolfemikl@gmail.com"
-    git init
-    git remote add origin https://github.com/chiefmikey/chalet-le-jar.git
+    sudo apt install -y wget unzip git gitsome jq awscli
     wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.34.02.zip
     unzip bedrock-server-1.17.34.02.zip
     rm bedrock-server-1.17.34.02.zip
-    wget -O ~/awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
-    unzip ~/awscliv2.zip
-    sudo ~/aws/install
-    pw=$(aws secretsmanager get-secret-value --secret-id profile-languages | jq -r '.SecretString' | jq '.TOKEN')
+    pw=$(aws secretsmanager get-secret-value --secret-id repo | jq -r '.SecretString' | jq '.repo')
 
 
     git init
     git config --global user.name "chiefmikey"
     git config --global user.email "wolfemikl@gmail.com"
+    git remote add origin https://github.com/chiefmikey/chalet-le-jar.git
     git add permissions.json server.properties whitelist.json
 
     git pull origin main -f
