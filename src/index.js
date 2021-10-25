c2a6434731a7904b074cb310f31373e1a202a21e
chiefmikey <wolfemikl@gmail.com>
2021-10-24 23:08:42 -0600

    Update styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import axios from 'axios';
+import './styles.css';
+
+const test = async () => {
+  try {
+    const app = document.getElementById('app');
+    app.innerHTML = 'Loading...';
+    app.style.backgroundColor = '#000000';
+    const res = await axios.get('https://api.github.com/users/chiefmikey');
+    return app.appendChild(document.createTextNode(`${res.data}`));
+  } catch (e) {
+    return e;
+  }
+};
+
+test();
 
 const app = document.getElementById('app');
+app.innerHTML = 'Loading...';
+app.style.backgroundColor = '#000000';
 app.appendChild(document.createTextNode('Hello World'));
