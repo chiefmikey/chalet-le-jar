edcec0505866f3601d19cf14be3e0578de894475
chiefmikey <wolfemikl@gmail.com>
2021-10-26 07:35:13 -0600

    Update src index from js to jsx

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-// import axios from 'axios';
-// import './styles.css';
-
-document.getElementById('app').innerHTML = 'Hello World';
-
-console.log('something');
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import { h, render } from 'preact';
+import App from './components/App.jsx';
+import './styles/index.css';
+
+render(<App />, document.body);
