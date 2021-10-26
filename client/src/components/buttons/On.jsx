db6df46107d883e8096e29f99c1e444a67908ef3
chiefmikey <wolfemikl@gmail.com>
2021-10-26 07:37:12 -0600

    Refactor on functionality to component

---
+++ client/src/components/buttons/On.jsx
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+import axios from 'axios';
+import state from '../../helpers/state.js';
+
+const dns = false;
+const server = false;
+
+const submitOn = async () => {
+  // aws sdk turn on ec2 instance dns
+  const startUp = await state('START');
+  console.log(startUp);
+  // repeat dns healthcheck until success
+  // while (!startUp.StartingInstances)
+  // when dns is true, turn on server
+  // server healthcheck until success
+  // when server is true
+  // await axios.post('/api/on');
+};
+
+const On = () => (
+  <button id="button-on" onClick={submitOn}>
+    On
+  </button>
+);
+
+export default On;
