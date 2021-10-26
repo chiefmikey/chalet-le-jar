3b776e03ae68c06afa3a1b8c33c83e35b3dc4a67
chiefmikey <wolfemikl@gmail.com>
2021-10-26 07:35:31 -0600

    Init ec2 client

---
+++ client/src/libs/ec2Client.js
@@ -0,0 +1,14 @@
+import { EC2Client } from '@aws-sdk/client-ec2';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
+
+const REGION = 'us-east-2';
+const IDENTITY_POOL_ID = 'us-east-2:9de629c9-c774-42ef-b641-d06eb430f8d8';
+const ec2Client = new EC2Client({
+  region: REGION,
+  credentials: fromCognitoIdentityPool({
+    clientConfig: { region: REGION }, // Configure the underlying CognitoIdentityClient.
+    identityPoolId: IDENTITY_POOL_ID,
+  }),
+});
+
+export default ec2Client;
