9b1e7b83e537296d3baa51929f6b1f75d963a815
chiefmikey <wolfemikl@gmail.com>
2021-10-26 22:39:26 -0600

    Init smoothscroll polyfill

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import { h, render } from 'preact';
 import App from './components/App.jsx';
 import './styles/index.css';
+import smoothscroll from '../../node_modules/smoothscroll-polyfill/dist/smoothscroll.js';
+
+smoothscroll.polyfill();
 
 render(<App />, document.body);
