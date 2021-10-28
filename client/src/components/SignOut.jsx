9dd1d97952dd7e7dbef541a9e24f633e349e247f
chiefmikey <wolfemikl@gmail.com>
2021-10-28 12:24:11 -0600

    Add await to auth init

--- client/src/components/SignOut.jsx
+++ client/src/components/SignOut.jsx
@@ -2,8 +2,8 @@ import { h } from 'preact';
 
 const signOutUser = async () => {
   try {
-    await window.gapi.load('auth2', () => {
-      window.gapi.auth2.init();
+    await window.gapi.load('auth2', async () => {
+      await window.gapi.auth2.init();
     });
     const auth2 = await window.gapi.auth2.getAuthInstance();
     await auth2.signOut();
