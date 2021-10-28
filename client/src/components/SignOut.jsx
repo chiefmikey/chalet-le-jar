8ae8d71364ce7ee693781e37eac8f6f8b72d1452
chiefmikey <wolfemikl@gmail.com>
2021-10-28 12:22:58 -0600

    Refactor signOutUser to await

--- client/src/components/SignOut.jsx
+++ client/src/components/SignOut.jsx
@@ -1,19 +1,17 @@
 import { h } from 'preact';
 
-const signOutUser = () => {
-  window.gapi.load('auth2', () => {
-    window.gapi.auth2.init();
-  });
-  if (window.gapi && window.gapi.auth2) {
-    const auth2 = window.gapi.auth2.getAuthInstance();
-    auth2
-      .signOut()
-      .then(() => {
-        console.log('User signed out');
-        document.getElementsByClassName('g-signin2').style.display = 'visible';
-        document.getElementById('lock-screen').style.display = 'visible';
-      })
-      .catch((e) => console.log('Sign out user error', e));
+const signOutUser = async () => {
+  try {
+    await window.gapi.load('auth2', () => {
+      window.gapi.auth2.init();
+    });
+    const auth2 = await window.gapi.auth2.getAuthInstance();
+    await auth2.signOut();
+    console.log('User signed out');
+    document.getElementsByClassName('g-signin2').style.display = 'visible';
+    document.getElementById('lock-screen').style.display = 'visible';
+  } catch (e) {
+    console.log('Sign out user error', e);
   }
 };
 
