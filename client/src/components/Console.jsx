100824811186895dfcac91b29de0bea9a41c4b32
chiefmikey <wolfemikl@gmail.com>
2021-10-28 13:44:31 -0600

    Move log listener into rendered component

--- client/src/components/Console.jsx
+++ client/src/components/Console.jsx
@@ -1,26 +1,27 @@
 import { h } from 'preact';
 import { console } from 'console-events';
 
-console.addEventListener('log', (e) => {
-  let newline = '/';
-  const consoleWindow = document.getElementById('console');
-  if (e.arguments.length > 0) {
-    for (let i = 0; i < e.arguments.length; i += 1) {
-      consoleWindow.append(`${e.arguments[i]} `);
-      for (let j = e.arguments[i].length; j < 97; j += 1) {
-        newline += '-';
+const Console = () => {
+  console.addEventListener('log', (e) => {
+    let newline = '/';
+    const consoleWindow = document.getElementById('console');
+    if (e.arguments.length > 0) {
+      for (let i = 0; i < e.arguments.length; i += 1) {
+        consoleWindow.append(`${e.arguments[i]} `);
+        for (let j = e.arguments[i].length; j < 97; j += 1) {
+          newline += '-';
+        }
+        newline += '/ ';
+        consoleWindow.append(newline);
+        newline = '/';
+        consoleWindow.scroll({
+          top: consoleWindow.scrollHeight,
+          behavior: 'smooth',
+        });
       }
-      newline += '/ ';
-      consoleWindow.append(newline);
-      newline = '/';
-      consoleWindow.scroll({
-        top: consoleWindow.scrollHeight,
-        behavior: 'smooth',
-      });
     }
-  }
-});
-
-const Console = () => <div id="console" />;
+  });
+  return <div id="console" />;
+};
 
 export default Console;
