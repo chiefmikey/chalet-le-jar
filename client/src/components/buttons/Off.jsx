2187b15e9080591740bfba189c2e73f8433ca976
chiefmikey <wolfemikl@gmail.com>
2021-10-30 10:31:40 -0600

    Refactor off button to component

---
+++ client/src/components/buttons/Off.jsx
@@ -0,0 +1,57 @@
+import { h } from 'preact';
+import axios from 'axios';
+import state from '../../helpers/state.js';
+
+const dns = false;
+const server = false;
+
+const dnsHealthCheck = async () => {
+  const health = await axios.get(`${dns}/health`);
+  if (health.data.status === 'UP') {
+    return true;
+  }
+  return false;
+};
+
+const serverHealthCheck = async () => {
+  const health = await axios.get(`${server}/health`);
+  if (health.data.status === 'UP') {
+    return true;
+  }
+  return false;
+};
+
+const submitOff = async (token) => {
+  // aws sdk turn on ec2 instance dns
+  const turnOff = await state('STOP', token);
+  console.log(turnOff);
+  // repeat dns healthcheck until success
+  // or get success response from turnOff/state
+  // let dnsHealth = false;
+  // while (!dnsHealth) {
+  //   dnsHealth = dnsHealthCheck();
+  // }
+  // while (!turnOff.StartingInstances)
+  // when dns is true, turn on server
+  // server healthcheck until success
+  // when server is true
+  // await axios.post('/api/on');
+};
+
+const Off = ({ lightUp, lightOff, token }) => (
+  <button
+    id="button-off"
+    onClick={async (ev) => {
+      ev.preventDefault();
+      lightUp(ev);
+      await submitOff(token);
+      lightOff(ev);
+    }}
+  >
+    <div className="button-text">
+      <h5>OFF</h5>
+    </div>
+  </button>
+);
+
+export default Off;
