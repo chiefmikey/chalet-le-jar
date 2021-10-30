bd66553de5d18772004f1e29c9f77609bd6bdd85
chiefmikey <wolfemikl@gmail.com>
2021-10-30 10:31:01 -0600

    Add token argument

--- client/src/helpers/state.js
+++ client/src/helpers/state.js
@@ -6,28 +6,28 @@ import launchClient from '../libs/ec2Client.js';
 
 const params = { InstanceIds: ['i-0ca1ce46c83788324', 'i-0c043740e90887911'] };
 
-const state = async (command) => {
-  if (await launchClient()) {
-    if (command.toUpperCase() === 'START') {
-      try {
-        const data = await launchClient().send(
-          new StartInstancesCommand(params),
-        );
-        console.log('Success', data.StartingInstances);
+const state = async (command, token) => {
+  if (command.toUpperCase() === 'START') {
+    try {
+      const launch = await launchClient(token);
+      if (launch) {
+        const data = await launch.send(new StartInstancesCommand(params));
+        console.log('Success - starting instances', data.StartingInstances);
         return data;
-      } catch (e) {
-        console.log('Error2', e);
       }
-    } else if (process.argv[2].toUpperCase() === 'STOP') {
-      try {
-        const data = await launchClient().send(
-          new StopInstancesCommand(params),
-        );
-        console.log('Success', data.StoppingInstances);
+    } catch (e) {
+      console.log('Error2', e);
+    }
+  } else if (command.toUpperCase() === 'STOP') {
+    try {
+      const launch = await launchClient(token);
+      if (launch) {
+        const data = await launch.send(new StopInstancesCommand(params));
+        console.log('Success - stopping instances', data.StoppingInstances);
         return data;
-      } catch (e) {
-        console.log('Error', e);
       }
+    } catch (e) {
+      console.log('Error', e);
     }
   }
   return null;
