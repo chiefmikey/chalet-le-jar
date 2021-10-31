41e7c17a8d556738f9ab395dea32fd59173c60cd
chiefmikey <wolfemikl@gmail.com>
2021-10-30 23:28:51 -0600

    Update console dev switch

--- client/src/components/Console.jsx
+++ client/src/components/Console.jsx
@@ -1,28 +1,58 @@
 import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import { console } from 'console-events';
 
-const Console = () => {
-  console.addEventListener('log', (e) => {
-    let newline = '/';
-    const consoleWindow = document.getElementById('console');
-    if (consoleWindow && e.arguments.length > 0) {
-      for (let i = 0; i < e.arguments.length; i += 1) {
-        const statement = `${JSON.stringify(e.arguments[i])} `;
-        consoleWindow.append(statement);
-        for (let j = statement.length; j < 80; j += 1) {
-          newline += '-';
+const Console = ({ statement }) => {
+  useEffect(() => {
+    console.addEventListener('log', (e) => {
+      if (e.arguments.length > 0) {
+        const devConsoleWindow = document.getElementById('dev-console');
+        const prodConsoleWindow = document.getElementById('prod-console');
+        if (devConsoleWindow && prodConsoleWindow && e.arguments.length > 0) {
+          for (let i = 0; i < e.arguments.length; i += 1) {
+            const devStatement = `${JSON.stringify(e.arguments[i])}\n`;
+            const prodStatement = `${e.arguments[i]}\n`;
+            devConsoleWindow.append(devStatement);
+            devConsoleWindow.scroll({
+              top: devConsoleWindow.scrollHeight,
+              behavior: 'smooth',
+            });
+            prodConsoleWindow.append(prodStatement);
+            prodConsoleWindow.scroll({
+              top: prodConsoleWindow.scrollHeight,
+              behavior: 'smooth',
+            });
+          }
         }
-        newline += '/ ';
-        consoleWindow.append(newline);
-        newline = '/';
-        consoleWindow.scroll({
-          top: consoleWindow.scrollHeight,
-          behavior: 'smooth',
-        });
       }
-    }
+    });
+  }, []);
+
+  useEffect(() => {
+    const devConsoleWindow = document.getElementById('dev-console');
+    const prodConsoleWindow = document.getElementById('prod-console');
+    devConsoleWindow.scroll({
+      top: devConsoleWindow.scrollHeight,
+      behavior: 'smooth',
+    });
+    prodConsoleWindow.scroll({
+      top: prodConsoleWindow.scrollHeight,
+      behavior: 'smooth',
+    });
   });
-  return <div id="console" />;
+
+  return (
+    <div id="console">
+      <div
+        id="prod-console"
+        style={statement !== 'prod' && { display: 'none' }}
+      />
+      <div
+        id="dev-console"
+        style={statement !== 'dev' && { display: 'none' }}
+      />
+    </div>
+  );
 };
 
 export default Console;
