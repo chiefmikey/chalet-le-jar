8cbc13736d77d514b94aea3f8b7f8106d5b0e7d6
chiefmikey <wolfemikl@gmail.com>
2021-11-01 07:48:19 -0600

    Update off button console log

--- client/src/components/buttons/Off.jsx
+++ client/src/components/buttons/Off.jsx
@@ -5,35 +5,8 @@ import state from '../../helpers/state.js';
 const dns = false;
 const server = false;
 
-const dnsHealthCheck = async () => {
-  const health = await axios.get(`${dns}/health`);
-  if (health.data.status === 'UP') {
-    return true;
-  }
-  return false;
-};
-
-const serverHealthCheck = async () => {
-  const health = await axios.get(`${server}/health`);
-  if (health.data.status === 'UP') {
-    return true;
-  }
-  return false;
-};
-
 const submitOff = async (token) => {
   console.log('Shutting down...');
-  setInterval(async () => {
-    try {
-      const turnOff = await state('STOP', token);
-      for (let i = 0; i < turnOff.length; i += 1) {
-        console.log('Instance 1: ', turnOff[i].previousState.name);
-        console.log('Instance 2: ', turnOff[i].currentState.name);
-      }
-    } catch (e) {
-      console.log('error in STOP command', e);
-    }
-  }, 1000);
   // repeat dns healthcheck until success
   // or get success response from turnOff/state
   // let dnsHealth = false;
