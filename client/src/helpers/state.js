80900f4bfc329c9c1e64fe1bd85db492e16e3df1
chiefmikey <wolfemikl@gmail.com>
2021-11-01 08:09:20 -0600

    Refactor with interval function

--- client/src/helpers/state.js
+++ client/src/helpers/state.js
@@ -6,28 +6,25 @@ import launchClient from '../libs/ec2Client.js';
 
 const params = { InstanceIds: ['i-0ca1ce46c83788324', 'i-0c043740e90887911'] };
 
+const interval = (data) =>
+  setInterval(() => {
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(
+        'Previous State: ',
+        data[i].previousState.name,
+        'Current State: ',
+        data[i].currentState.name,
+      );
+    }
+    return data;
+  }, 5000);
+
 const startInstances = async (token) => {
   try {
     const launch = await launchClient(token);
     if (launch) {
       const data = await launch.send(new StartInstancesCommand(params));
-      setInterval(async () => {
-        try {
-          const turnOn = await state('START', token);
-          for (let i = 0; i < turnOn.length; i += 1) {
-            console.log(
-              'Previous State: ',
-              turnOn[i].previousState.name,
-              'Current State: ',
-              turnOn[i].currentState.name,
-            );
-          }
-        } catch (e) {
-          console.log('Error starting instances', e);
-          return data;
-        }
-        return null;
-      }, 5000);
+      interval(data);
     }
   } catch (e) {
     console.log('Error launching AWS client', e);
@@ -40,23 +37,7 @@ const stopInstances = async (token) => {
     const launch = await launchClient(token);
     if (launch) {
       const data = await launch.send(new StopInstancesCommand(params));
-      setInterval(async () => {
-        try {
-          const turnOff = await state('STOP', token);
-          for (let i = 0; i < turnOff.length; i += 1) {
-            console.log(
-              'Previous State: ',
-              turnOff[i].previousState.name,
-              'Current State: ',
-              turnOff[i].currentState.name,
-            );
-          }
-        } catch (e) {
-          console.log('Error stopping instances', e);
-          return data;
-        }
-        return data;
-      }, 5000);
+      interval(data);
     }
   } catch (e) {
     console.log('Error launching AWS client', e);
