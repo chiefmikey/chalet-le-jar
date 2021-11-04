ba68ea80c558024918b0d35d112446a0e34ca7c7
chiefmikey <wolfemikl@gmail.com>
2021-11-04 09:59:54 -0600

    Remove dev condition

--- client/src/libs/ec2Client.js
+++ client/src/libs/ec2Client.js
@@ -6,18 +6,16 @@ const IDENTITY_POOL_ID = 'us-east-2:9de629c9-c774-42ef-b641-d06eb430f8d8';
 
 const launchClient = async (ID_TOKEN) => {
   try {
-    if (ID_TOKEN) {
-      return await new EC2Client({
-        region: REGION,
-        credentials: fromCognitoIdentityPool({
-          clientConfig: { region: REGION },
-          identityPoolId: IDENTITY_POOL_ID,
-          logins: {
-            'accounts.google.com': ID_TOKEN,
-          },
-        }),
-      });
-    }
+    return await new EC2Client({
+      region: REGION,
+      credentials: fromCognitoIdentityPool({
+        clientConfig: { region: REGION },
+        identityPoolId: IDENTITY_POOL_ID,
+        logins: {
+          'accounts.google.com': ID_TOKEN,
+        },
+      }),
+    });
   } catch (e) {
     console.log('launchClient error', e);
   }
