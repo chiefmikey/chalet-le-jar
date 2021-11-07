63b960203923849d38bf3a1c83d69de9b8b5e183
chiefmikey <wolfemikl@gmail.com>
2021-11-06 20:27:41 -0600

    Update server scripts

--- scripts/server-stop.sh
+++ scripts/server-stop.sh
@@ -7,6 +7,7 @@ currentDate=$(date +%y-%m-%d-%H-%M-%S)
 git checkout main
 git add worlds
 git commit -am $currentDate
+git pull --no-edit origin main
 git push origin main
 git checkout -b $currentDate
 git push origin $currentDate
