e48c9dc58c834f6e7c878775d4f2c74672965fa2
chiefmikey <wolfemikl@gmail.com>
2021-11-09 14:27:45 -0700

    Update branches and buttons

--- docs/client/src/components/Branch.jsx
+++ docs/client/src/components/Branch.jsx
@@ -3,14 +3,13 @@ import { h } from 'preact';
 
 const Branch = ({ branches, submitBranch }) =>
   branches.map((branch) => (
-    <li
-      key={branch.name}
-      className="branch"
-      onClick={(e) => {
-        e.target.className('selected-branch');
-        submitBranch(e);
-      }}
-    >{`${branch}`}</li>
+    <li key={branch} className="branch">
+      <button
+        type="button"
+        className="branch-button"
+        onClick={submitBranch}
+      >{`${branch}`}</button>
+    </li>
   ));
 
 export default Branch;
