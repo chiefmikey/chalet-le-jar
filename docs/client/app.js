ab935dd8b33b72bd81a477ac9bc35ce9b397033e
chiefmikey <wolfemikl@gmail.com>
2021-11-10 15:23:42 -0700

    Update with unicorn rules

--- docs/client/app.js
+++ docs/client/app.js
@@ -2,7 +2,7 @@ import Koa from 'koa';
 // import bodyParser from 'koa-bodyparser';
 // import cors from '@koa/cors';
 import serve from 'koa-static';
-import path from 'path';
+import path from 'node:path';
 
 const port = 3000;
 
--- docs/client/src/helpers/submit.js
+++
@@ -1,8 +0,0 @@
-import { branchSelected } from '../components/buttons/Rewind.jsx';
-
-const onSubmit = (e, token, branch) => {
-  e.preventDefault();
-  branchSelected(e, token, branch);
-};
-
-export default onSubmit;
