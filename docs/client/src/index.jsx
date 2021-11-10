96fe21ad09d8d0958bc029f9e66f1ab74f3580c5
chiefmikey <wolfemikl@gmail.com>
2021-11-10 10:13:10 -0700

    Include css reset

--- docs/client/src/index.jsx
+++ docs/client/src/index.jsx
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import { h, render } from 'preact';
 import App from './components/App.jsx';
-import './styles/index.css';
 import 'airbnb-browser-shims';
+import 'the-new-css-reset/css/reset.css';
+import './styles/index.css';
 
 render(<App />, document.body);
