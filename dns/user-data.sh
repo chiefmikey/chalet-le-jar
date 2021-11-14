f3725d26fdf945122c774861130418e859cdf56d
chiefmikey <wolfemikl@gmail.com>
2021-11-13 21:56:38 -0700

    Update dns data

--- dns/user-data.sh
+++ dns/user-data.sh
@@ -26,14 +26,19 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
     yum update -y
     yum install -y wget unzip git
     rm -rf /usr/local/go
-    wget -O /home/ec2-user/go.tar.gz https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+    wget -O /home/ec2-user/go.tar.gz https://golang.org/dl/go1.17.3.linux-amd64.tar.gz
     tar -C /usr/local -xzf /home/ec2-user/go.tar.gz
     rm /home/ec2-user/go.tar.gz
-    echo "export PATH=$PATH:/usr/local/go/bin" >> /home/ec2-user/.profile
-    source /home/ec2-user/.profile
+    mkdir /home/ec2-user/go
+    chown -R ec2-user:root /home/ec2-user/go
+    echo "export GOPATH=/home/ec2-user/go" >> /home/ec2-user/.bashrc
+    echo "export PATH=$PATH:$GOPATH/bin" >> /home/ec2-user/.bashrc
+    echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+    source /etc/profile
     git clone https://github.com/coredns/coredns
+    chown -R ec2-user:root /home/ec2-user/coredns
     cd /home/ec2-user/coredns
-    make
+    su -s /bin/bash -c 'make' ec2-user
     echo \
       ".:53 {
         rewrite name exact mco.lbsg.net ip.chaletlejar.com
@@ -41,11 +46,11 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
       }" \
     >> /home/ec2-user/coredns/Corefile
     chmod +x /home/ec2-user/coredns/coredns
-    sudo su -s /bin/bash -c 'screen -S coredns -dm sudo /home/ec2-user/coredns/coredns' root
+    su -s /bin/bash -c 'screen -S coredns -dm sudo /home/ec2-user/coredns/coredns' root
 else
   cd /home/ec2-user
   echo "-- Not first instance startup --"
-    sudo yum update -y
-    sudo su -s /bin/bash -c 'screen -S coredns -dm sudo /home/ec2-user/coredns/coredns' root
+    yum update -y
+    su -s /bin/bash -c 'screen -S coredns -dm sudo /home/ec2-user/coredns/coredns' root
 fi
 --//--
