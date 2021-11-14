2fa68ba6c8d4e56e3302ccd816843e4196636ae6
chiefmikey <wolfemikl@gmail.com>
2021-11-14 15:42:18 -0700

    Update stop script

--- scripts/server-stop.sh
+++ scripts/server-stop.sh
@@ -2,14 +2,17 @@
 
 cd /home/ubuntu
 screen -S bedrock -X stuff "save hold\n"
-sleep 5
+sleep 10
 currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
-git add worlds log
+zip -r /home/ubuntu/worlds.zip /home/ubuntu/worlds
+git add /home/ubuntu/worlds.zip /home/ubuntu/log
+git restore --staged /home/ubuntu/worlds
 git stash push
 git checkout -b $currentDate
 git stash pop
 echo "Shutdown: $currentDate" >> /home/ubuntu/log/shutdown-log.txt
 git commit -am $currentDate
 git push origin $currentDate
+rm /home/ubuntu/worlds.zip
 screen -S bedrock -X stuff "save resume\n"
 screen -S bedrock -X stuff "stop\n"
