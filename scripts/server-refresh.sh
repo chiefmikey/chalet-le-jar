b795b5df56d24be911d618b0ca381c7643f983df
chiefmikey <wolfemikl@gmail.com>
2021-11-14 16:51:24 -0700

    Update refresh commit log

--- scripts/server-refresh.sh
+++ scripts/server-refresh.sh
@@ -9,18 +9,18 @@ zip -r worlds.zip worlds
 git add worlds.zip log
 git restore --staged worlds
 git stash push
-git checkout -b $currentDate
+git checkout -b refresh-$currentDate
 git stash pop
 echo "Refresh: $currentDate" >> log/refresh-log.txt
-git commit -am $currentDate
-git push origin $currentDate
+git commit -am refresh-$currentDate
+git push origin refresh-$currentDate
 rm worlds.zip
 screen -S bedrock -X stuff "save resume\n"
 screen -S bedrock -X stuff "stop\n"
 git fetch --all
 git reset --hard origin/main
 rm -R worlds log
-git checkout origin/$currentDate worlds.zip log
+git checkout origin/refresh-$currentDate worlds.zip log
 unzip worlds.zip
 rm worlds.zip
 LD_LIBRARY_PATH=/home/ubuntu su -s /bin/bash -c 'screen -S bedrock -dm /home/ubuntu/bedrock_server' root
