25e2ebfedc49b8d06fdbe9108de33832b96e74f7
chiefmikey <wolfemikl@gmail.com>
2021-11-14 14:44:36 -0700

    Update save script

--- scripts/server-save.sh
+++ scripts/server-save.sh
@@ -4,11 +4,13 @@ cd /home/ubuntu
 screen -S bedrock -X stuff "save hold\n"
 sleep 10
 currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
-git add worlds log
+mv /home/ubuntu/worlds /home/ubuntu/world
+git add world log
 git stash push
 git checkout -b $currentDate
 git stash pop
 echo "Save: $currentDate" >> /home/ubuntu/log/save-log.txt
 git commit -am $currentDate
 git push origin $currentDate
+mv /home/ubuntu/world /home/ubuntu/worlds
 screen -S bedrock -X stuff "save resume\n"
