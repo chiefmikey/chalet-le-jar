83c2a0b0fcc3e29fd67aab361ede6f53ca463baa
chiefmikey <wolfemikl@gmail.com>
2021-11-16 22:13:29 -0700

    Update scripts

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -7,9 +7,9 @@ pw=$(aws secretsmanager --region us-east-2 get-secret-value --secret-id repo | j
 sleep 10
 git remote remove origin
 git remote add origin https://chalet-le-jar:${pw}@github.com/chiefmikey/chalet-le-jar.git
-git remote update
 git fetch --all
 git checkout main
+git pull origin main
 git reset --hard origin/main
 rm -r worlds log
 git checkout origin/$latest worlds log
