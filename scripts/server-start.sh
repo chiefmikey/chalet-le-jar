75cc79743c626401fd4373bea467298b18ee649a
chiefmikey <wolfemikl@gmail.com>
2021-11-16 22:34:12 -0700

    Update scripts

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -3,18 +3,15 @@
 cd /home/ubuntu
 apt update -y
 apt upgrade -y
-pw=$(aws secretsmanager --region us-east-2 get-secret-value --secret-id repo | jq -r ".SecretString" | jq -r ".repo")
-sleep 10
-git remote remove origin
-git remote add origin https://chalet-le-jar:${pw}@github.com/chiefmikey/chalet-le-jar.git
+# pw=$(aws secretsmanager --region us-east-2 get-secret-value --secret-id repo | jq -r ".SecretString" | jq -r ".repo")
+# sleep 10
+# git remote remove origin
+# git remote add origin https://chalet-le-jar:${pw}@github.com/chiefmikey/chalet-le-jar.git
 git fetch --all
-git checkout main
-git pull origin main
 git reset --hard origin/main
 rm -r worlds log
 git checkout origin/$latest worlds log
 unzip worlds/clj/data.zip
 rm worlds/clj/data.zip
-echo "Latest: $latest" >> log/startup-log.txt
 LD_LIBRARY_PATH=/home/ubuntu su -s /bin/bash -c 'screen -S bedrock -dm /home/ubuntu/bedrock_server' root
 su -s /bin/bash -c 'screen -S watch -dm watch /home/ubuntu/scripts/server-autosave.sh' root
