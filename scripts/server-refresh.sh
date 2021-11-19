708426185ca31e84523190ad030335da8ebbd832
chiefmikey <wolfemikl@gmail.com>
2021-11-19 14:04:06 -0700

    Update unzip with overwrite

--- scripts/server-refresh.sh
+++ scripts/server-refresh.sh
@@ -19,7 +19,7 @@ rm worlds/clj/data.zip
 git fetch --all
 git reset --hard origin/main
 git checkout origin/$currentDate-refresh worlds log
-unzip worlds/clj/data.zip
+unzip -o worlds/clj/data.zip
 rm worlds/clj/data.zip
 LD_LIBRARY_PATH=/home/ubuntu su -s /bin/bash -c 'screen -S bedrock -dm /home/ubuntu/bedrock_server' root
 su -s /bin/bash -c 'screen -S watch -dm watch /home/ubuntu/scripts/server-autosave.sh' root
