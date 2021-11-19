adc68b8fe4e3ddb11007b89577d536c2bc0e8e4c
chiefmikey <wolfemikl@gmail.com>
2021-11-19 14:00:19 -0700

    Remove echo

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -9,7 +9,6 @@ git remote remove origin
 git remote add origin https://chalet-le-jar:${pw}@github.com/chiefmikey/chalet-le-jar.git
 git fetch --all
 git reset --hard origin/main
-echo $latest
 git checkout origin/$latest worlds log
 unzip worlds/clj/data.zip
 rm worlds/clj/data.zip
