e3d8d6465460738a402fb53bb22de36d2501f4d4
chiefmikey <wolfemikl@gmail.com>
2021-11-19 20:37:08 -0700

    Update dns instance id

--- docs/environment.js
+++ docs/environment.js
@@ -1,6 +1,6 @@
 const environment = {
   serverInstanceId: 'i-0d42af2d2c4d177ac',
-  dnsInstanceId: 'i-05bd5f7d4fafe2fe7',
+  dnsInstanceId: 'i-0a08084ac7b591d61',
   region: 'us-east-2',
   identityPoolId: 'us-east-2:9de629c9-c774-42ef-b641-d06eb430f8d8',
   secretName: 'repo',
