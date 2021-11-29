11173e81b5f9a8e96a48c0fcb10648a29dd85f45
chiefmikey <wolfemikl@gmail.com>
2021-11-29 15:12:51 -0700

    Set interval to make sure sign in button renders

--- docs/client/src/components/SignIn.jsx
+++ docs/client/src/components/SignIn.jsx
@@ -2,21 +2,34 @@
 import { h } from 'preact';
 import { useEffect } from 'preact/hooks';
 
+const signInButton = (onSignIn) =>
+  window.gapi.signin2.render('lock-screen', {
+    scope: 'email openid',
+    width: 400,
+    height: 100,
+    longtitle: true,
+    theme: 'dark',
+    onsuccess: onSignIn,
+  });
+
 const SignIn = ({ onSignIn }) => {
   useEffect(() => {
-    const lockScreen = document.createElement('div');
-    lockScreen.setAttribute('id', 'lock-screen');
-    document.querySelector('#app').append(lockScreen);
-    if (window.gapi) {
-      window.gapi.signin2.render('lock-screen', {
-        scope: 'email openid',
-        width: 400,
-        height: 100,
-        longtitle: true,
-        theme: 'dark',
-        onsuccess: onSignIn,
-      });
-    }
+    const appendSignIn = async () => {
+      const lockScreen = document.createElement('div');
+      lockScreen.setAttribute('id', 'lock-screen');
+      document.querySelector('#app').append(lockScreen);
+      if (window.gapi) {
+        signInButton(onSignIn);
+      } else {
+        const checkWindow = setInterval(() => {
+          if (window.gapi) {
+            signInButton(onSignIn);
+            clearInterval(checkWindow);
+          }
+        }, 100);
+      }
+    };
+    appendSignIn();
   });
 };
 
