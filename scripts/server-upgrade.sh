d95002311e1eebc51574de36c86d8b29f8f1ff51
chiefmikey <wolfemikl@gmail.com>
2021-12-05 11:28:09 -0700

    Update upgrade script to run in refresh condition

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -2,43 +2,13 @@
 
 cd /home/ubuntu
 currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
-echo "Refresh: $currentDate" >> log/refresh-log.txt
-screen -S watch -X quit
-screen -S bedrock -X stuff "playsound beacon.activate @a\n"
-sleep 1
-screen -S bedrock -X stuff "say refreshing in 10...9...8...7...6...5...4...3...2...1...\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound beacon.deactivate @a\n"
-sleep 1
-screen -S bedrock -X stuff "stop\n"
-sleep 10
-killall screen
-sudo wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.1.02.zip
-sudo unzip -o bedrock-server.zip
-sudo rm bedrock-server.zip
-sudo git fetch origin main
-sudo git add permissions.json server.properties whitelist.json
-sudo git checkout main
-sudo git reset --hard origin/main
+wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.1.02.zip
+unzip -o bedrock-server.zip
+rm bedrock-server.zip
+git fetch origin main
+git add permissions.json server.properties whitelist.json
+git checkout main
+git reset --hard origin/main
 LD_LIBRARY_PATH=/home/ubuntu su -s /bin/bash -c 'screen -S bedrock -dm /home/ubuntu/bedrock_server' root
 su -s /bin/bash -c 'screen -S watch -dm watch /home/ubuntu/scripts/server-autosave.sh' root
-sudo currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
-sudo echo "Upgrade: $currentDate" >> log/upgrade-log.txt
+echo "Upgrade: $currentDate" >> log/upgrade-log.txt
