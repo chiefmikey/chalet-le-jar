344541c207b2815454926ee531d0d2bd6e198b78
chiefmikey <wolfemikl@gmail.com>
2022-02-23 18:53:15 -0700

    Remove log from server start

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -3,7 +3,6 @@
 cd /home/ubuntu
 apt update -y
 apt upgrade -y
-currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
 pw=$(aws secretsmanager --region us-east-2 get-secret-value --secret-id repo | jq -r ".SecretString" | jq -r ".repo")
 sleep 10
 git remote remove origin
@@ -14,4 +13,3 @@ git reset --hard origin/main
 killall screen
 LD_LIBRARY_PATH=/home/ubuntu su -s /bin/bash -c 'screen -S bedrock -dm /home/ubuntu/bedrock_server' root
 su -s /bin/bash -c 'screen -S watch -dm watch /home/ubuntu/scripts/server-autosave.sh' root
-echo "Start: $currentDate" >> log/startup-log.txt
