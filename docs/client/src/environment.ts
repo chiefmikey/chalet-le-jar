dfff5f0d66c2c768c9c94036417f627de0d1ec3d
Mikl Wolfe <wolfemikl@gmail.com>
2022-02-27 22:51:05 -0600

    Update environment.ts

--- docs/client/src/environment.ts
+++ docs/client/src/environment.ts
@@ -1,6 +1,6 @@
 const environment = {
   serverInstanceId: 'i-0d42af2d2c4d177ac',
-  dnsInstanceId: 'i-02a638ec954303a5a',
+  dnsInstanceId: 'i-09640b64556a6fb77',
   region: 'us-east-2',
   identityPoolId: 'us-east-2:9de629c9-c774-42ef-b641-d06eb430f8d8',
   secretName: 'repo',
