772315f98342c30e1d157c411c5d1faa5ad5bbd8
chiefmikey <wolfemikl@gmail.com>
2022-03-03 21:40:09 -0700

    Include user data local

---
+++ scripts/user-data-local.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+sudo cd /home/chalet-le-jar
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y wget zip unzip git jq awscli curl
+sudo wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.12.01.zip
+sudo unzip -o bedrock-server.zip
+sudo rm bedrock-server.zip
+export user="chalet-le-jar"
+export email="chaletlejar@gmail.com"
+export repo="chalet-le-jar"
+export owner="chiefmikey"
+export awsRegion="us-east-2"
+export awsSecretId="repo"
+export pw=$(sudo aws secretsmanager --region ${awsRegion} get-secret-value --secret-id ${awsSecretId} | jq -r ".SecretString" | jq -r ".${awsSecretId}")
+sudo sleep 10
+sudo git init
+sudo git config user.name ${user}
+sudo git config user.email ${email}
+sudo git remote add origin https://${user}:${pw}@github.com/${owner}/${repo}.git
+sudo git fetch origin main
+sudo git reset --hard origin/main
+sudo git checkout main
+sudo chmod +x scripts/server-save.sh scripts/server-stop.sh scripts/server-refresh.sh scripts/server-rewind.sh scripts/server-push.sh scripts/server-upgrade.sh
+sudo mkdir /home/chalet-le-jar/backups
+sudo mkdir /home/chalet-le-jar/backups/save
+sudo mkdir /home/chalet-le-jar/backups/autosave
+sudo mkdir /home/chalet-le-jar/backups/backup
+sudo mkdir /home/chalet-le-jar/worlds
+sudo chown -R chalet-le-jar:root /home/chalet-le-jar
+sudo currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
+sudo echo "Init: $currentDate" >> logs/init-log.txt
+sudo git add logs
+sudo git commit -am "Server initialized"
+sudo git push origin main
