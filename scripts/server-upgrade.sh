5f30c54066d32a31c9eeae2b9d6f0bd3007ca76f
chiefmikey <wolfemikl@gmail.com>
2022-06-08 14:10:03 -0600

    Update server upgrade version

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -2,7 +2,7 @@
 
 cd /home/chalet-le-jar
 currentDate=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
-wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.33.02.zip
+wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.1.01.zip
 unzip -o bedrock-server.zip
 rm bedrock-server.zip
 git fetch origin main
