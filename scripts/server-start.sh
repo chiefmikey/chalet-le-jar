14e700ede86fe990a27b1e1c782cee7655d128a6
Mikl Wolfe <wolfemikl@gmail.com>
2022-06-29 18:31:27 -0600

    Update server-start.sh

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -8,9 +8,18 @@ apt update -y
 apt upgrade -y
 apt autoremove -y
 git remote remove origin
-export pw=$(aws secretsmanager --region us-east-2 get-secret-value --secret-id repo | jq -r ".SecretString" | jq -r ".repo")
+export user="chalet-le-jar"
+export email="chaletlejar@gmail.com"
+export repo="chalet-le-jar"
+export owner="chiefmikey"
+export awsRegion="us-east-2"
+export awsSecretId="repo"
+export pw=$(aws secretsmanager --region ${awsRegion} get-secret-value --secret-id ${awsSecretId} | jq -r ".SecretString" | jq -r ".${awsSecretId}")
 sleep 10
-git remote add origin https://chalet-le-jar:${pw}@github.com/chiefmikey/chalet-le-jar.git
+git init
+git config user.name ${user}
+git config user.email ${email}
+git remote add origin https://${user}:${pw}@github.com/${owner}/${repo}.git
 git fetch --prune
 git checkout main
 git reset --hard origin/main
