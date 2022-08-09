9e402d1fbb8fd51865f9ed2fdc6182888d7de265
chiefmikey <wolfemikl@gmail.com>
2022-08-08 20:27:43 -0600

    Update time stamp format

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -2,7 +2,7 @@
 
 set -x
 cd /home/chalet-le-jar
-export CURRENT_DATE=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
+export CURRENT_DATE=$(TZ=":US/Mountain" date +%y-%m-%d_%H-%M-%S)
 wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.11.01.zip
 unzip -o bedrock-server.zip
 rm bedrock-server.zip
--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -2,7 +2,7 @@
 
 set -x
 cd /home/ubuntu
-export CURRENT_DATE=$(TZ=":US/Mountain" date +%y-%m-%d-%H-%M-%S)
+export CURRENT_DATE=$(TZ=":US/Mountain" date +%y-%m-%d_%H-%M-%S)
 apt update -y
 apt upgrade -y
 apt install -y wget zip unzip git jq awscli curl
