d62894ea301a8c313b2058c80552c1c0004d1783
chiefmikey <wolfemikl@gmail.com>
2022-10-13 18:06:16 -0600

    Update dir

--- scripts/server-backup.sh
+++ scripts/server-backup.sh
@@ -3,7 +3,7 @@
 set -x
 cd /home/chalet-le-jar
 export CURRENT_DATE=$(TZ=":US/Mountain" date +%y-%m-%d_%H-%M-%S)
-cp -r worlds/clj backups/backup/${CURRENT_DATE}
+cp -r 'worlds/Chalet le Jar' backups/backup/${CURRENT_DATE}
 git add "backups/backup/${CURRENT_DATE}"
 git stash push
 git checkout -b "backup/${CURRENT_DATE}"
