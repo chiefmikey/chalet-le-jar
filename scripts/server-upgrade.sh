d1b81e29e393f6780fcf6a887fcbb60b8b9cc215
Mikl Wolfe <wolfemikl@gmail.com>
2022-11-11 09:59:15 -0700

    Update server-upgrade.sh

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -9,8 +9,11 @@ rm bedrock-server.zip
 git fetch --prune
 git checkout main
 git reset --hard origin/main
+echo "# upgrade" > /home/chalet-le-jar/upgrade.txt
+git commit -am "Disable upgrade: ${CURRENT_DATE}"
+git push origin main
 CURRENT_DATE=${CURRENT_DATE} ACTION=upgrade \
-  /home/chalet-le-jar/scripts/server-log.sh upgrade
+  /home/chalet-le-jar/scripts/server-log.sh
 if [ "${START_SCREENS}" = "y" ]; then
   LD_LIBRARY_PATH=/home/chalet-le-jar screen -S bedrock -dm /home/chalet-le-jar/bedrock_server
   screen -S autosave -dm /home/chalet-le-jar/scripts/server-autosave.sh
