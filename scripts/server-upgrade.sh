9d685f6eb5c0b4c760739f9ba39c5bb4bf7fbbf6
Mikl Wolfe <wolfemikl@gmail.com>
2022-11-11 09:51:54 -0700

    Update server-upgrade.sh

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -10,7 +10,7 @@ git fetch --prune
 git checkout main
 git reset --hard origin/main
 CURRENT_DATE=${CURRENT_DATE} ACTION=upgrade \
-  /home/chalet-le-jar/scripts/server-log.sh
+  /home/chalet-le-jar/scripts/server-log.sh upgrade
 if [ "${START_SCREENS}" = "y" ]; then
   LD_LIBRARY_PATH=/home/chalet-le-jar screen -S bedrock -dm /home/chalet-le-jar/bedrock_server
   screen -S autosave -dm /home/chalet-le-jar/scripts/server-autosave.sh
