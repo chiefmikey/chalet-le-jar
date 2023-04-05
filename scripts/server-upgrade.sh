8c44bb10356f78d878d544db6185c2ac0a69de4a
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-05 17:01:33 -0600

    feature(upgrade): 1.19.73.02

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -4,7 +4,7 @@ set -x
 cd /home/chalet-le-jar || exit
 CURRENT_DATE=$(TZ=:US/Mountain date +%m-%d-%y_%H:%M:%S)
 export CURRENT_DATE
-wget -O bedrock-server.zip "https://minecraft.azureedge.net/bin-linux-preview/bedrock-server-1.19.80.24.zip"
+wget -O bedrock-server.zip "https://minecraft.azureedge.net/bin-linux-preview/bedrock-server-1.19.73.02.zip"
 unzip -o bedrock-server.zip
 rm bedrock-server.zip
 git fetch --prune
