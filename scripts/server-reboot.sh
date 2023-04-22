004a90b1c6d3037ef08ec591ac674a56e4b88134
Mikl Wolfe <wolfemikl@gmail.com>
2023-04-22 10:31:17 -0600

    feature(server-reboot): update script

--- scripts/server-reboot.sh
+++ scripts/server-reboot.sh
@@ -1,40 +1,4 @@
 #!/bin/sh
 
 set -x
-cd /home/chalet-le-jar || exit
-CURRENT_DATE=$(TZ=:US/Mountain date +%m-%d-%y_%H:%M:%S)
-export CURRENT_DATE
-screen -S autosave -X quit
-screen -S bedrock -X stuff "playsound beacon.activate @a\n"
-sleep 1
-screen -S bedrock -X stuff "say stopping in 10...9...8...7...6...5...4...3...2...1...\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a\n"
-sleep 1
-screen -S bedrock -X stuff "playsound beacon.deactivate @a\n"
-sleep 1
-screen -S bedrock -X stuff "stop\n"
-sleep 10
-killall screen
-cp -r worlds/clj backups/autosave/"${CURRENT_DATE}"
-CURRENT_DATE=${CURRENT_DATE} ACTION=reboot \
-  /home/chalet-le-jar/scripts/server-log.sh
-cd /home/chalet-le-jar/backups/autosave || exit
-ls -1t | tail -n +73 | xargs -d "\n" rm -rf
 reboot
