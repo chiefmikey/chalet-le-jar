cda92a9b29167d6b66a5dfeb47e60a1b8144c329
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-21 10:19:37 -0700

    feat(server): update script

--- scripts/server-ts.sh
+++ scripts/server-ts.sh
@@ -12,14 +12,18 @@ if ! command -v svn > /dev/null 2>&1; then
   apt install -y subversion
 fi
 if ! command -v node > /dev/null 2>&1; then
-  curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-  apt install -y nodejs
+  apt update
+  apt install -y ca-certificates curl gnupg
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  NODE_MAJOR=20
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+  apt update
+  apt install nodejs -y
 fi
 if ! command -v npm > /dev/null 2>&1; then
   apt install -y npm
 fi
-
-sudo apt install npm
 
 svn export https://github.com/chiefmikey/chalet-le-jar/trunk/server --force
 
