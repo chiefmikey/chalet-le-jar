6318f5af91b7b3f55b3d595e60cbfa6e23fb11f6
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-22 16:44:00 -0700

    cat

--- scripts/server-countdown.sh
+++ scripts/server-countdown.sh
@@ -10,59 +10,59 @@ screen -S bedrock -X stuff "say stopping in 10...9...8...7...6...5...4...3...2..
 sleep 1
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
-
-
-screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
-screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
 screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
-sleep .2
+sleep .5
 screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
-sleep .8
+sleep .5
 
 
-screen -S bedrock -X stuff "playsound beacon.deactivate @a ~ ~ ~ 1 .5\n"
+screen -S bedrock -X stuff "playsound mob.cat.straymeow @a ~ ~ ~ 1 .5\n"
+sleep .5
+screen -S bedrock -X stuff "stopsound @a mob.cat.straymeow\n"
+sleep .5
+
+
+screen -S bedrock -X stuff "playsound beacon.deactivate @a\n"
 sleep 1
 
 screen -S bedrock -X stuff "stop\n"
--- scripts/server-ticking.sh
+++ scripts/server-ticking.sh
@@ -5,6 +5,8 @@ set -x
 screen -S bedrock -X stuff "tickingarea add circle 142 90 169 4 KELP\n"
 screen -S bedrock -X stuff "tickingarea add circle 0 0 0 4 GUARDIAN\n"
 screen -S bedrock -X stuff "tickingarea add circle 1290 188 -10 4 CREEPER\n"
+screen -S bedrock -X stuff "tickingarea add circle 27 64 167 4 CASTLE\n"
 screen -S bedrock -X stuff "tickingarea preload KELP true\n"
 screen -S bedrock -X stuff "tickingarea preload GUARDIAN true\n"
 screen -S bedrock -X stuff "tickingarea preload CREEPER true\n"
+screen -S bedrock -X stuff "tickingarea preload CASTLE true\n"
