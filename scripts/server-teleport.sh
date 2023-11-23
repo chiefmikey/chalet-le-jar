1d0cfaffdd2812633cdbbe42d7adaae1ce899122
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-22 17:10:45 -0700

    add sleep

--- scripts/server-teleport.sh
+++ scripts/server-teleport.sh
@@ -5,4 +5,5 @@ set -x
 screen -S bedrock -X stuff "tickingarea add circle ${2} 1 ${1}\n"
 sleep 2
 screen -S bedrock -X stuff "tp ${1} ${2} true\n"
+sleep 2
 screen -S bedrock -X stuff "tickingarea remove ${1}\n"
