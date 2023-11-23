7672687d414fd4010dd257e04f483adb45023191
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-22 17:05:53 -0700

    sleep 2

--- scripts/server-teleport.sh
+++ scripts/server-teleport.sh
@@ -3,5 +3,6 @@
 source /home/chalet-le-jar/.bash_aliases
 set -x
 screen -S bedrock -X stuff "tickingarea add circle ${2} 1 ${1}\n"
+sleep 2
 screen -S bedrock -X stuff "tp ${1} ${2} true\n"
 screen -S bedrock -X stuff "tickingarea remove ${1}\n"
