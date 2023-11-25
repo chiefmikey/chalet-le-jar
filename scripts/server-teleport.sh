b034229b8b7d31757c87830c2af1d6c5b85d8f37
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-25 13:00:25 -0700

    fix(scripts): update teleport tick state delays

--- scripts/server-teleport.sh
+++ scripts/server-teleport.sh
@@ -3,7 +3,7 @@
 source /home/chalet-le-jar/.bash_aliases
 set -x
 screen -S bedrock -X stuff "tickingarea add circle ${2} 1 ${1}\n"
-sleep 2
+sleep 1
 screen -S bedrock -X stuff "tp ${1} ${2} true\n"
-sleep 2
+sleep 5
 screen -S bedrock -X stuff "tickingarea remove ${1}\n"
