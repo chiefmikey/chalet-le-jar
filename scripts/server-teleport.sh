dfaffca882b45cbf2daf742ac5a4bdafc077cde4
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-26 11:44:23 -0700

    fix(scripts): escape teleport variables

--- scripts/server-teleport.sh
+++ scripts/server-teleport.sh
@@ -2,8 +2,8 @@
 
 source /home/chalet-le-jar/.bash_aliases
 set -x
-screen -S bedrock -X stuff "tickingarea add circle ${2} 1 ${1}\n"
+screen -S bedrock -X stuff "tickingarea add circle \"${2}\" 1 \"${1}\"\n"
 sleep 1
-screen -S bedrock -X stuff "tp ${1} ${2} true\n"
+screen -S bedrock -X stuff "tp \"${1}\" \"${2}\" true\n"
 sleep 5
-screen -S bedrock -X stuff "tickingarea remove ${1}\n"
+screen -S bedrock -X stuff "tickingarea remove \"${1}\"\n"
