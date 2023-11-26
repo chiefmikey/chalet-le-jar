d776528deb3e544f8878e573758bb2429c050dac
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-26 13:59:36 -0700

    fix(scripts): update username escaping

--- scripts/server-ticking-add.sh
+++ scripts/server-ticking-add.sh
@@ -2,4 +2,4 @@
 
 source /home/chalet-le-jar/.bash_aliases
 set -x
-screen -S bedrock -X stuff "tickingarea add circle ${1} ${2} ${3}\n"
+screen -S bedrock -X stuff "tickingarea add circle ${1} ${2} \"${3}\"\n"
--- scripts/server-ticking-preload.sh
+++ scripts/server-ticking-preload.sh
@@ -2,4 +2,4 @@
 
 source /home/chalet-le-jar/.bash_aliases
 set -x
-screen -S bedrock -X stuff "tickingarea preload ${1} true\n"
+screen -S bedrock -X stuff "tickingarea preload \"${1}\" true\n"
