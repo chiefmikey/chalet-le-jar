acd36e7c91cc792454f1ec371cd8305f70217bf7
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-06 09:19:15 -0700

    Update server-upgrade.sh

--- scripts/server-upgrade.sh
+++ scripts/server-upgrade.sh
@@ -4,7 +4,7 @@ source /home/chalet-le-jar/.bash_aliases
 set -x
 cd "${ROOT}" || exit
 CURRENT_DATE=$(TZ=:US/Mountain date +%m-%d-%y_%H:%M:%S)
-wget -O bedrock-server.zip "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.20.41.02.zip"
+wget -O bedrock-server.zip "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.20.50.03.zip"
 unzip -o bedrock-server.zip
 rm bedrock-server.zip
 git fetch --prune
