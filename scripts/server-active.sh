1eb56917bc0eb6c04e6e845b7a5cb77d00b0becc
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-18 14:24:02 -0700

    fix(scripts): update server active

--- scripts/server-active.sh
+++ scripts/server-active.sh
@@ -39,6 +39,14 @@ declare -a ACTIVE_USERS=()
 # function to update the users.log file
 update_users_log() {
   printf "%s\n" "${ACTIVE_USERS[@]}" > "${USERS_LOG}"
+
+  # if there is at least one active user, start the autosave
+  if [ ${#ACTIVE_USERS[@]} -ge 1 ]; then
+    start_autosave
+  # if there are no active users, stop the autosave
+  elif [ ${#ACTIVE_USERS[@]} -eq 0 ]; then
+    stop_autosave
+  fi
 }
 
 # monitor bedrock screenlog for player spawn and disconnect events
@@ -47,13 +55,16 @@ update_users_log() {
     if [[ $line == *"Player connected:"* ]]; then
       # extract the username and add it to the array
       username=${line#*"Player connected: "}
+      username=${username%%,*}  # new line to remove everything after the comma
       ACTIVE_USERS+=("$username")
       echo "Player connected: $username"
       update_users_log
     elif [[ $line == *"Player disconnected:"* ]]; then
       # extract the username and remove it from the array
       username=${line#*"Player disconnected: "}
-      ACTIVE_USERS=("${ACTIVE_USERS[@]/$username}")
+      username=${username%%,*}  # new line to remove everything after the comma
+      ACTIVE_USERS=( "${ACTIVE_USERS[@]/$username}" ) # remove the user
+      ACTIVE_USERS=( "${ACTIVE_USERS[@]}" ) # re-index the array
       echo "Player disconnected: $username"
       update_users_log
     fi
